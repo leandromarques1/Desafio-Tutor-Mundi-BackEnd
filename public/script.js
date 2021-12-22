b32f812c523ee33ed1a7cad46e9ff4a232f599df
Anderson Leandro Marques <43223824+leandromarques1@users.noreply.github.com>
2021-12-22 12:06:14 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -21,34 +21,48 @@ const star5 = document.getElementById('estrela5')
 //--> criar um Array com os nomes de cada Id das estrelas
 var star = [star1,star2,star3,star4,star5]
 
+
+const buttonConfirm = document.getElementById('tm-button')
+
+function notaAtribuida(posicao)
+{
+    //Parametro "posicao" é a posição da Estrela clicado no Array
+    var nota = posicao + 1
+    
+    //ao clicar no botão CONFIRMAR    
+    buttonConfirm.onclick = function ()
+    {
+        alert('A nota atribuída foi de ' + nota + ' estrelas')
+        for(i=0; i<=posicao; i++)
+        {
+            star[i].src='images/estrela_vazia.svg'
+            buttonConfirm.style.background = '#C8C8C8'
+        }
+    }
+} 
+
+
 function clickStar(element)
 {
-    if(star.includes(element)) //se TRUE
-    {   
-        //--> identificar em qual posição do meu array está localizado o elemento que possue valor igual a "element". Essa posição receberá o nome N
-        var N = star.indexOf(element)
-        //--> será feito um FOR, que irá iterar N vezes. Cada uma dessa iterações acenderá uma estrela
-        for(i=0; i<=N;i++)
-        {
-            star[i].src='images/estrela_cheia.svg'
-        }
-        const buttonConfirm = document.getElementById('tm-button')
-        buttonConfirm.style.background = '#2CC6D0'
-        var nota = N + 1
-        
-        buttonConfirm.onclick = function ()
-        {
-            alert('A nota atribuída foi de ' + nota + ' estrelas')
-            for(i=0; i<=N;i++)
-            {
-                star[i].src='images/estrela_vazia.svg'
-                buttonConfirm.style.background = '#C8C8C8'
-            }
-        }
+    //--> identificar em qual posição do meu array está localizado o elemento 
+    //    que possue valor igual a "element". Essa posição receberá o nome N
+    var N = star.indexOf(element)
+    
+    //--> será feito um FOR, que irá iterar N vezes. 
+    //    Cada uma dessa iterações acenderá uma estrela até o Número N
+    for(i=0; i<=N;i++)
+    {
+        star[i].src='images/estrela_cheia.svg'
     }
-    else
-    {   //se FALSE
-        alert('Dê sua anota clicando em uma das estrelas')
+    //ao mesmo tempo, irá apagar as estrelas seguintes (5-i)
+    for(i=N+1; i<5;i++)
+    {
+        star[i].src='images/estrela_vazia.svg'
     }
-}   
+
+    //const buttonConfirm = document.getElementById('tm-button')
+    buttonConfirm.style.background = '#2CC6D0'
+    notaAtribuida(N)
+     
+}  
 
